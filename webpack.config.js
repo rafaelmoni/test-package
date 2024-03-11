3984f5d11b61f77cfd5ac107814203a5781d61aa
Rafael Moni <rafaelmoni@hotmail.com>
2024-03-11 18:45:39 -0300

    Added webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    chunkFilename: '[name].bundle.js',
+    publicPath: isProduction ? '/dist/' : '/',
+    libraryTarget: 'commonjs2',
+    globalObject: 'this',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [],
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+    next: 'next',
+    'next/router': 'next/router',
+  },
+  //devtool: isProduction ? false : 'eval-source-map', // 'source-map'
+}
